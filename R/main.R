dd9cefe60281c4c7128f48e8f73d4ee846d6d2ba
kennedynyavor <kennedy.nyavor@gmail..com>
2025-01-05 01:10:03 -0600

    Function Added

--- R/main.R
+++ R/main.R
@@ -1,6 +1,66 @@
 library(lubridate)
+library(tidyverse)
+library(janitor)
 
-read_pmt <- function(path,prod_date){
+#' A function that imports all payment
+#'
+#' @param prod_month The production month
+#'
+#' @returns A data frame of all payment data
+#' @export
+#'
+#' @import lubridate
+#'
+#' @import tidyverse
+#'
+#' @import janitor
+#'
+#' @examples
+#' prod_month <- ymd('2024-11-01')
+#' read_pmt(prod_month)
+read_pmt <- function(prod_month) {
+
+month_year <- paste0(toupper(month(prod_month, label = TRUE)), year(prod_month))
+root_folder <- paste0("c:/Users/kknya/Documents/z_data/data_prep/raw_data/",
+                      month_year,
+                      "/")
+check_file <- str_detect(dir(root_folder), paste0("PAYMENT", ".*", month_year, ".txt$"))
 
 
+file_path <- paste0(root_folder, dir(root_folder)[check_file])
+
+df <- read_delim(file_path,
+                 delim = ";",
+                 trim_ws = TRUE,
+                 name_repair = make_clean_names) %>%
+  mutate_at(vars(matches("date$")), mdy) %>%
+  mutate(
+    agent_branch = if_else(str_detect(agent_branch, "NABCO"), agent_team, agent_branch),
+    agent_branch = if_else(str_detect(agent_branch, "TEAM 2"), "KUMASI", agent_branch),
+    agent_branch = if_else(
+      str_detect(agent_branch, "NON AGENTS"),
+      "TELESALES",
+      agent_branch
+    )
+  ) %>%
+  filter(str_detect(
+    str_sub(policy_number, 1, 4),
+    "EMFP|EMSP|EFPP|EPPP|PDG0",
+    negate = TRUE
+  )) %>%
+  group_by(policy_number) %>%
+  mutate(inception_date = min(payment_date)) %>%
+  ungroup() %>%
+  mutate(
+    channel =
+      case_when(
+        str_detect(agent_branch, "BANCASSU") ~ "Banca",
+        str_detect(agent_branch, "PRU.*ADVISOR") ~ "PruAdvisor",
+        str_detect(agent_branch, "TELESALES") ~ "TELESALES",
+        str_detect(agent_branch, "BROKER|RISCOVERY|CONSULT") ~ "Broker",
+        .default = "Agency"
+
+      )
+  )
+return(df)
 }
